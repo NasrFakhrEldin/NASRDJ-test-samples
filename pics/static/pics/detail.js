4acc308d346e7c21fc9e196289dc44cb049059de
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-05-10 08:08:47 +0200

    error - pic not found

--- pics/static/pics/detail.js
+++ pics/static/pics/detail.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
-    onclick = document.getElementById('overlay').style.display = 'none';
-    onclick = document.getElementById('overlay').style.display = 'block';
-})
+// document.addEventListener('DOMContentLoaded', function(){
+//     onclick = document.getElementById('overlay').style.display = 'none';
+//     onclick = document.getElementById('overlay').style.display = 'block';
+// })
