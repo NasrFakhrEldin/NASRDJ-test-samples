5900e8b3866ab7dcd803f75071b10a1da09cf836
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-05-11 11:00:55 +0200

    finish most views - templates

--- forums/urls.py
+++ forums/urls.py
@@ -3,5 +3,9 @@ from . import views
 app_name = 'forums'
 
 urlpatterns = [
-    
+    path('', views.ForumListView.as_view(), name = 'all'),
+    path('create', views.ForumCreateView.as_view(), name = 'forum_create'),
+    path('<int:pk>', views.ForumDetailView.as_view(), name = 'forum_detail'),
+    path('<int:pk>/update', views.ForumUpdateView.as_view(), name = 'forum_update'),
+    path('<int:pk>/delete', views.ForumDeleteView.as_view(), name = 'forum_delete'),
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render, HttpResponse
-
+from .owners import *
+from .models import Forum, Comment
+from django.urls import reverse, reverse_lazy
+from .forms import CommentForm
+from django.shortcuts import render
 # Create your views here.
 
+
+class ForumCreateView(OwnerCreateView): # using model not form as pics app
+    model = Forum
+    template_name = 'forums/form.html'
+    fields = ['title', 'text']
+    success_url = reverse_lazy('forums:all')
+
+class ForumListView(OwnerListView):
+    model = Forum
+    template_name = 'forums/list.html'
+
+
+class ForumDetailView(OwnerDetailView):
+    model = Forum
+    template_name = 'forums/detail.html'
+
+    def get(self, request, pk):
+        forum = Forum.objects.get(id=pk)
+        comments = Comment.objects.filter(forum=forum).order_by('-updated_at')
+        comment_form = CommentForm()
+        return render(request, self.template_name, {
+            "forum" : forum,
+            "comments" : comments,
+            "comment_form" : comment_form,
+        })
+
+
+class ForumUpdateView(OwnerUpdateView):
+    model = Forum
+    template_name = 'forums/form.html'
+    fields = ['title', 'text']
+    success_url = reverse_lazy('forums:all')
+
+class ForumDeleteView(OwnerDeleteView):
+    model = Forum
+    template_name = 'forums/delete.html'
+    success_url = reverse_lazy('forums:all')
