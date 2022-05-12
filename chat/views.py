4b2bec56acf25ad3aa8f7ea855330b84757cd019
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-05-12 16:51:23 +0200

    create class SendMessage to test it

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from .models import Message
 # Create your views here.
 
 def index(request):
     return HttpResponse('hi')
 
-# class SendMessage(LoginRequiredMixin, View):
+class SendMessage(LoginRequiredMixin, View):
+    
+    def get(self, request):
+        return render(request, ".html")
+    
+    def post(self, request):
+        message = Message(text = request.POST['message'], owner=request.user)
+        message.save()
+        return redirect(reverse('chat:'))
